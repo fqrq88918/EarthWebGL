4cb27b009c4be87ff7c151338c6d06ed18f65ae8
fqrq88918 <530192386+fqrq88918@users.noreply.github.com>
2018-06-29 17:00:11 +0800

    11

---
+++ qiu/Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CreateAssetBundles
+{
+    // 打包unity场景文件
+    [MenuItem("Assets/Build AssetBundle Scene")]
+    static void MyBuild()
+    {
+        // 需要打包的场景名字
+        string[] levels = { "Assets/Scene/earth.unity" };
+        // 注意这里【区别】通常我们打包，第2个参数都是指定文件夹目录，在此方法中，此参数表示具体【打包后文件的名字】
+        // 记得指定目标平台，不同平台的打包文件是不可以通用的。最后的BuildOptions要选择流格式
+        BuildPipeline.BuildPlayer(levels, Application.dataPath + "/earth.unity3d", BuildTarget.WebGL, BuildOptions.BuildAdditionalStreamedScenes);
+        // 刷新，可以直接在Unity工程中看见打包后的文件
+        AssetDatabase.Refresh();
+    }
+}
---
+++ qiu/Assets/Scripts/LoadSceneBundle.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// 脚本位置：需要加载物体的场景中任意物体上
+/// 脚本功能：加载物体
+/// </summary>
+public class LoadSceneBundle : MonoBehaviour
+{
+        
+    private string url;
+    private string assetname;
+ 
+    void Start ()
+    {
+        // 下载压缩包，写出具体的名字
+       //url = "file://" + Application.dataPath + "/earth.unity3d";
+        url = Application.absoluteURL + "/../earth.unity3d";
+        // unity预制体名字，即被打包的场景名字叫 2
+        assetname = "earth";
+ 
+        StartCoroutine (Download ());
+    }
+ 
+    IEnumerator Download ()
+    {
+ 
+        WWW www = new WWW (url);
+        yield return www;
+        if (www.error != null) {
+            Debug.Log (www.error);
+        } else {
+            AssetBundle bundle = www.assetBundle;
+            SceneManager.LoadSceneAsync ("earth");
+            print ("跳转场景");
+            // AssetBundle.Unload(false)，释放AssetBundle文件内存镜像，不销毁Load创建的Assets对象
+            // AssetBundle.Unload(true)，释放AssetBundle文件内存镜像同时销毁所有已经Load的Assets内存镜像
+            bundle.Unload (false);
+        }
+ 
+        // 中断正在加载过程中的WWW
+        www.Dispose ();
+    }
+ 
+}
