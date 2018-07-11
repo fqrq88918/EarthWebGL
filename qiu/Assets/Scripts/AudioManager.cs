c80b50fce0ec358910019b9fffbd41ccaa762548
fqrq88918 <530192386+fqrq88918@users.noreply.github.com>
2018-07-11 10:45:34 +0800

    111

---
+++ qiu/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour {
+    public Button audioPlayButtion;
+
+    public Button audioPauseButtion;
+
+    public AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () 
+    {
+        audioPlayButtion.onClick.AddListener(delegate
+        {
+            //Debug.Log("hahaha");
+            AudioPause();
+        });
+
+        audioPauseButtion.onClick.AddListener(delegate
+        {
+
+            AudioPlay();
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void AudioPlay()
+    {
+        audioPlayButtion.gameObject.SetActive(true);
+        audioPauseButtion.gameObject.SetActive(false);
+        audioSource.Play();
+    }
+
+    void AudioPause()
+    {
+        audioPlayButtion.gameObject.SetActive(false);
+        audioPauseButtion.gameObject.SetActive(true);
+        audioSource.Pause();
+    }
+}
---
+++ qiu/Assets/Scripts/TextAnimaManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TextAnimaManager : MonoBehaviour {
+    public Text chinaText;
+
+    public Text englishText;
+	// Use this for initialization
+	void Start ()
+    {
+        DoAnima();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void DoAnima()
+    {
+        chinaText.GetComponent<RectTransform>().DOAnchorPos3D(new Vector3(0f,-20f,0f), 4f);
+        chinaText.GetComponent<RectTransform>().DOScale(Vector3.one * 0.1f,4f).OnComplete(
+            delegate
+            {
+            englishText.GetComponent<RectTransform>().DOScale(Vector3.one, 2f).SetEase(Ease.OutBack);
+                englishText.DOFade(1f, 2f);
+            }
+        );
+       // DOTween.To(() => chinaText.fontSize, x => chinaText.fontSize = x, 18, 3f)
+
+    }
+}
