0a91d7610a2c9dbc68c83e4f4799cf9eb959dbb8
fqrq88918 <530192386+fqrq88918@users.noreply.github.com>
2018-07-24 14:25:58 +0800

    111

--- qiu/Assets/Scripts/EarthInitAnima.cs
+++ qiu/Assets/Scripts/EarthInitAnima.cs
@@ -19,27 +19,27 @@ public class EarthInitAnima : MonoBehaviour {
                 text_2nd.transform.localScale = Vector3.one;
                 text_2nd.gameObject.SetActive(true);
                 text_1st.gameObject.SetActive(false);
-                text_2nd.transform.DOLocalRotate(new Vector3(0f, 180f, 0f), 1f).OnComplete(delegate {
-
-                    text_2nd.transform.DOScale(0f,0f).SetDelay(2f).OnComplete(delegate {
-                        childCamera.parent = null;
-                        text_2nd.SetActive(false);
-                        text_1st.transform.DOPause();
-                        text_1st.transform.localScale = Vector3.zero;
-                        text_1st.transform.localPosition = new Vector3(-4.49f, 3.45f, 0.06f);
-                        text_1st.transform.localEulerAngles = new Vector3(15.95f, 176.13f, -2.2f);
-                        text_1st.gameObject.SetActive(true);
-                        text_1st.transform.DOScale(Vector3.one * 0.7f, 1f).SetEase(Ease.OutBack);
-                        ui_2D.DOFade(1f, 2f);
-                        CameraViewer.getInstance.isInitAnimaComplete = true;
-                        CameraViewer.getInstance.StartRotate = false;
-                    });
-
-                });
+               
 
                
         });
+            text_2nd.transform.DOLocalRotate(new Vector3(0f, 180f, 0f), 3f).OnComplete(delegate {
 
+                text_2nd.transform.DOScale(0f, 0f).SetDelay(2f).OnComplete(delegate {
+                    childCamera.parent = null;
+                    text_2nd.SetActive(false);
+                    text_1st.transform.DOPause();
+                    text_1st.transform.localScale = Vector3.zero;
+                    text_1st.transform.localPosition = new Vector3(-4.49f, 3.45f, 0.06f);
+                    text_1st.transform.localEulerAngles = new Vector3(15.95f, 176.13f, -2.2f);
+                    text_1st.gameObject.SetActive(true);
+                    text_1st.transform.DOScale(Vector3.one * 0.7f, 1f).SetEase(Ease.OutBack);
+                    ui_2D.DOFade(1f, 2f);
+                    CameraViewer.getInstance.isInitAnimaComplete = true;
+                    CameraViewer.getInstance.StartRotate = false;
+                });
+
+            });
         });
 	}
 	
